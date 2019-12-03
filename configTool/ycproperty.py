3875b30f1f147125ec65765cf315daaa85207c81
GM-yyr <398916756@qq.com>
2019-12-03 16:25:54 +0800

    Update ycproperty.py

--- configTool/ycproperty.py
+++ configTool/ycproperty.py
@@ -5,8 +5,8 @@ from flask import Blueprint, request
 from iceCon import ice_con
 import json
 import Ice
-Ice.loadSlice("./ice-sqlite.ice")
-# Ice.loadSlice("/code/tool/configTool/ice-sqlite.ice")
+# Ice.loadSlice("./ice-sqlite.ice")
+Ice.loadSlice("/code/tool/configTool/ice-sqlite.ice")
 import YCArea
 
 yc_blu = Blueprint('yc', __name__)
