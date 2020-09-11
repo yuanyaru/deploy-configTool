c41a69b21206df42c041987db22f07050df2a9a1
yuanyaru <398916756@qq.com>
2020-09-11 13:47:14 +0800

    update

--- configTool/static/js/common/return-top.js
+++ configTool/static/js/common/return-top.js
@@ -1,15 +1,15 @@
 window.onload = function(){
     var oTop = document.getElementById("to_top");
     //获取可视区域宽度
-    var screenw = document.documentElement.clientWidth || document.getElementById("section").clientWidth;
+    var screenw = document.documentElement.clientWidth || document.getElementById("tBody_yx").clientWidth;
     //获取可视区域高度
-    var screenh = document.documentElement.clientHeight || document.getElementById("section").clientHeight;
+    var screenh = document.documentElement.clientHeight || document.getElementById("tBody_yx").clientHeight;
     //确定按钮距离屏幕可视区域左边的位置
     oTop.style.left = screenw - oTop.offsetWidth +"px";
     oTop.style.top = screenh - oTop.offsetHeight +"px";
-    document.getElementById("section").onscroll = function(){
+    document.getElementById("tBody_yx").onscroll = function(){
         //获取滚动条的滚动高度
-        var scrolltop = document.documentElement.scrollTop || document.getElementById("section").scrollTop;
+        var scrolltop = document.documentElement.scrollTop || document.getElementById("tBody_yx").scrollTop;
         if(scrolltop>0){
             // alert("a");
             document.getElementById("to_top").style.display="block";
@@ -18,8 +18,8 @@ window.onload = function(){
         } else {
             document.getElementById("to_top").style.display="none";
         }
-    }
+    };
     oTop.onclick = function(){
-        document.documentElement.scrollTop = document.getElementById("section").scrollTop =0;
+        document.documentElement.scrollTop = document.getElementById("tBody_yx").scrollTop =0;
     }
-}
+};
