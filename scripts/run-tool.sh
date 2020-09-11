b97a0cb722dc829ecbd3f5b9010119f7629e266a
yuanyaru <398916756@qq.com>
2020-09-11 14:02:22 +0800

    update

--- scripts/run-tool.sh
+++ scripts/run-tool.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name configtool -p 5000:5000 -v ../deploy-configTool:/code/tool --restart always --privileged=true -it configtool
+docker run --name configtool -p 5000:5000 -v $PWD:/code/tool --restart always --privileged=true -it configtool
